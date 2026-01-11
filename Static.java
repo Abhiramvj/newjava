e924cfa606a75273da27d5331bede0b73b194027
Abhiramvj <abhiramsanthosh@iocod.com>
2026-01-11 22:31:34 +0530

    learned about static methods

--- Static.java
+++ Static.java
@@ -1,33 +1,36 @@
 class Mobile {
-    int price;
     static String name;
     String brand;
-    public Mobile() {
-        brand = "";
-        price= 200;
-        System.out.println("in constructore");
-    }
-
-    static {
-        name = "phone";
-        System.out.println("in statuc");
-    }
+    int price;
 
     public void show() {
-    System.out.println(name + " " + brand + " " + price);
-}
-}
+        System.out.println(name + " " + brand + " " + price);
+    }
 
+    public static void show1(Mobile obj1) {
+        System.out.println(name + " " + obj1.brand + " " + obj1.price);
+    }
 
+}
 
 public class Static {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("hii");
-        // Mobile obj1 = new Mobile();
-        // obj1.price = 1500;
-        // Mobile.name = "SmartPhone";
-        // obj1.brand = "Apple";
+    public static void main(String[] args) {
+        Mobile obj1 = new Mobile();
+        Mobile.name = "Phone";
+        obj1.brand = "Apple";
+        obj1.price = 1500;
 
+        Mobile obj2 = new Mobile();
+        Mobile.name = "SmartPhone";
+        obj2.brand = "Samsung";
+        obj2.price = 1300;
 
+        Mobile.name = "Shit";
+
+        obj1.show();
+        obj2.show();
+
+        Mobile.show1(obj1);
+        
     }
 }
