38c46991dd5ea882f8ccdc029e056d0109ca4c12
Abhiram Santhosh <150274570+Abhiramvj@users.noreply.github.com>
2026-01-13 19:20:41 +0530

    learned encapsulation

---
+++ encapsulation.java
@@ -0,0 +1,31 @@
+class Human {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+
+    }
+
+    public void setAge(int a) {
+        age = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+}
+
+public class encapsulation {
+    public static void main(String[] args) {
+        Human obj = new Human();
+        obj.setAge(10);
+        obj.setName("Heollo");
+
+        System.out.println(obj.getAge() + " " + obj.getName());
+    }
+}
